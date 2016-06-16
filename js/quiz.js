01c38db807dcb56ee28953184f407b0ec83e0824
rem{e}koh <git@remekoh.me>
2016-06-16 16:38:00 -0500

    Completed all functions

--- js/quiz.js
+++ js/quiz.js
@@ -1,86 +1,92 @@
 'use strict'
 
 $(document).ready(function() {
-  var currentQuestionIndex = 0;
-  var userCorrectTotal = 0;
-  var questions = [
-    {
-      questionText: 'What is the current season of Big Bang Theory?',
-      options: ['Season 14', 'Season 25', 'Season 8', 'Season 9'],
-      correctAnswer: 'Season 9'
-    },
-    {
-      questionText: 'How many episodes of Big Bang Theory are there?',
-      options: ['350', '206', '400', '207'],
-      correctAnswer: '207'
-    },
-    {
-      questionText: 'Who is Sheldon\'s roommate?',
-      options: ['Penny', 'Amy', 'Howard', 'Leonard'],
-      correctAnswer: 'Leonard'
-    },
-    {
-      questionText: 'Which of these is not a character in Big Bang Theory',
-      options: ['Penny', 'Amy', 'Howard', 'Leonard','Eleanor'],
-      correctAnswer: 'Eleanor'
-    },
-    {
-      questionText: 'What age did Sheldon begin college?',
-      options: ['8', '16', '12', '11'],
-      correctAnswer: '11'
+    var currentQuestionIndex = 0;
+    var userCorrectTotal = 0;
+    var questions = [{
+        questionText: 'What is the current season of Big Bang Theory?',
+        options: ['Season 14', 'Season 25', 'Season 9', 'Season 8'],
+        correctAnswer: 'Season 9'
+    }, {
+        questionText: 'How many episodes of Big Bang Theory are there?',
+        options: ['207', '350', '206', '400'],
+        correctAnswer: '207'
+    }, {
+        questionText: 'Who is Sheldon\'s roommate?',
+        options: ['Penny', 'Amy', 'Howard', 'Leonard'],
+        correctAnswer: 'Leonard'
+    }, {
+        questionText: 'Which of these is not a character in Big Bang Theory',
+        options: ['Raj', 'Bernadette', 'Stuart', 'Eleanor'],
+        correctAnswer: 'Eleanor'
+    }, {
+        questionText: 'What age did Sheldon begin college?',
+        options: ['8', '16', '12', '11'],
+        correctAnswer: '11'
     }];
 
     // When triggered this listener calls the new game function
     $('.start-game').click(function() {
-      currentQuestionIndex = newGame(questions[0], questions.length);
+        currentQuestionIndex = newGame(questions[0], questions.length);
     });
 
     // Loads first question when page loads
     currentQuestionIndex = userCorrectTotal = newGame(questions[0], questions.length);
 
     $('form').submit(function(event) {
-      event.preventDefault();
+        event.preventDefault();
 
-      var userAnswer = $('input[type = "radio"]:checked');
+        var userAnswer = $('input[type = "radio"]:checked + span');
 
-      if (userAnswer.length === 1) {
-        // Checks if user answr is correct
-        if (isAnswerCorrect(questions[currentQuestionIndex], userAnswer)) {
-          userCorrectTotal++;
+        if (userAnswer.length === 1) {
+            // Checks if user answr is correct
+            if (isAnswerCorrect(questions[currentQuestionIndex], userAnswer.text())) {
+                userCorrectTotal++;
+                console.log('userCorrectTotal:' + userCorrectTotal);
+            }
+            if (currentQuestionIndex === 4) {
+                $('.quiz-area').empty();
+                $('.quiz-area').append('You answered ' + userCorrectTotal + ' out of ' + questions.length);
+            } else {
+                //Setup for the next question
+                currentQuestionIndex++;
+                console.log('currentQuestionIndex: ' + currentQuestionIndex);
+                loadQuestion(questions[currentQuestionIndex], currentQuestionIndex + 1);
+                diplayQuestionTotal(currentQuestionIndex + 1, questions.length);
+                $('input[type = "radio"]').prop('checked', false); // Clear previous answer
+            }
+        } else {
+            alert("Please choose an Answer, any answer!");
         }
-        //Setup for the next question
-        currentQuestionIndex++;
-        console.log('currentQuestionIndex: ' + currentQuestionIndex);
-        loadQuestion(questions[currentQuestionIndex], currentQuestionIndex + 1);
-        diplayQuestionTotal(currentQuestionIndex + 1, questions.length);
-        $('input[type = "radio"]').prop('checked', false); // Clear previous answer
-      } else {
-        alert("Please choose an Answer, any answer!");
-      }
     });
 });
 
 // Given a question object returns true if user presents correct
 // answer
-function isAnswerCorrect(currentQuestion, usersAnswer) {
-  
-
+function isAnswerCorrect(currentQuestion, uAns) {
+    console.log(uAns);
+    if (uAns === currentQuestion.correctAnswer) {
+        // alert('CORRECT!');
+        return true;
+    } else {
+        return false;
+    }
 }
 
 // Displays number questions answered out of total
 function diplayQuestionTotal(currentQuestionNum, totalNumQuestion) {
- $('.question-out-of-total').text('You are on '+currentQuestionNum + ' out of ' + totalNumQuestion);
+    $('.question-out-of-total').text('You are on ' + currentQuestionNum + ' out of ' + totalNumQuestion);
 }
 
 // Given a question object loads the next question on screen
 function loadQuestion(currentQuestion, currentIndex) {
-  console.log('index: ' + currentIndex);
-  $('.question-header').text(currentIndex + '. ' + currentQuestion.questionText);
-  console.log(currentQuestion.questionText);
-  for(var i = 0; i < 4; i++) {
-    console.log(currentQuestion.options[i]);
-    $('#' + i + ' span').text(currentQuestion.options[i]);
-  }
+    console.log('index: ' + currentIndex);
+    $('.question-header').text(currentIndex + '. ' + currentQuestion.questionText);
+    console.log(currentQuestion.questionText);
+    for (var i = 0; i < 4; i++) {
+        console.log(currentQuestion.options[i]);
+        $('#' + i + ' span').text(currentQuestion.options[i]);
+    }
 }
 
 // Updates the user's score
@@ -90,10 +96,8 @@ function updateScore() {
 
 // Reset for a new game
 function newGame(firstQuestion, totalNumQuestion) {
-  loadQuestion(firstQuestion, 1);
-  diplayQuestionTotal(1, totalNumQuestion);
-  $('input[type = "radio"]').prop('checked', false); // Clear previous answer
-  return 0;
+    loadQuestion(firstQuestion, 1);
+    diplayQuestionTotal(1, totalNumQuestion);
+    $('input[type = "radio"]').prop('checked', false); // Clear previous answer
+    return 0;
 }
-
-
